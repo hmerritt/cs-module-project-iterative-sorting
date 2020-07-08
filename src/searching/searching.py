06c54eb9df5382b14e9b84a7021935be0bcf7a11
Harry Merritt <harry@merritt.es>
2020-07-09 00:40:36 +0100

    Implement binary search function

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,8 +9,22 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    start = 0
+    end = len(arr) - 1
+    found = False
 
+    while end >= start and not found:
+        middle = (start + end) // 2
+
+        if arr[middle] == target:
+            found = True
+            return middle
+
+        else:
+            if target < arr[middle]:
+                end = middle - 1
+            if target > arr[middle]:
+                start = middle + 1
 
     return -1  # not found
